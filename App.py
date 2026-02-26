e9eb92d1aac54ff5ebf51f6a81d51eed8c9ac6bd
monika malviya <monikamalviya9644@gmail.com>
2026-02-26 16:32:30 +0530

    Initial Commit

---
+++ App.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import pickle
+
+
+
+st.title("DiabetesPrediction Application")
+st.header("Machine Learning")
+with open("DiabetesPrediction.pkl", 'rb') as file:
+    model = pickle.load(file)
+
+# Pregnancies	
+# Glucose	BloodPressure	SkinThickness	Insulin	BMI	DiabetesPedigreeFunction	Age
+
+Pregnancies = st.number_input("Pregnancies ", min_value=0 , max_value= 17)
+Glucose = st.number_input("Glucose ", min_value=0 , max_value= 200)
+BloodPressure = st.number_input("BloodPressure ", min_value=0 , max_value= 125)
+SkinThickness = st.number_input("SkinThickness ", min_value=0 , max_value= 100)
+Insulin = st.number_input("Insulin ", min_value=0 , max_value= 850)
+BMI = st.number_input("BMI ", min_value=0.0 , max_value= 70.0)
+DiabetesPedigreeFunction = st.number_input("DiabetesPedigreeFunction ", min_value=0.0 , max_value= 3.0)
+Age = st.number_input("Age ", min_value=18 , max_value= 85)
+
+
+
+if st.button("predict"):
+    new_array = np.array([[Pregnancies,Glucose	,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+    prediction = model.predict(new_array)
+
+    if prediction == 0:
+        st.success("Negative")
+    else:
+        st.success("Positive")    
+    
